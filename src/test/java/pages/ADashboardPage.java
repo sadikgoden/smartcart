00bc20a89be4e759f9cac561abe8c926a11fb31f
sadik Goden <sadikgoden001@gmail.com>
2023-10-25 00:46:34 +0300

    first commit

---
+++ src/test/java/pages/ADashboardPage.java
@@ -0,0 +1,17 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class ADashboardPage {
+    public ADashboardPage (){ PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+
+}
+
+    git add .
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/sadikgoden/smartcart.git
+        git push -u origin main
