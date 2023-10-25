3bea138427197cb7ac7dc91e1aace51d55297ea6
sadik Goden <sadikgoden001@gmail.com>
2023-10-25 17:09:25 +0300

    qwwer

--- src/test/java/tests/US_001.java
+++ src/test/java/tests/US_001.java
@@ -1,4 +1,16 @@
 package tests;
 
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
 public class US_001 {
+    public class TC_01 {
+        @Test
+        public void US_001(){
+            //Ziyaretçi olarak "https://qa.smartcardlink.com/" adresine gidiniz
+            Driver.getDriver().get(ConfigReader.getProperty("sAdminUrl"));
+            Driver.closeDriver();
+        }
+
 }
